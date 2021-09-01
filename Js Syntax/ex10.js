c6e2003e660f3a76e02c7233ffff69b96b79c6ba
Mihai <haos-sah@mail.ru>
2021-09-01 22:48:41 +0300

    done ex 10

--- Js Syntax/ex10.js
+++ Js Syntax/ex10.js
@@ -1,10 +1,11 @@
 //Напишите функцию которая определяет и выводит в консоль все делитель заданного числа.
 
 const dividers = (number) => {
-    for (let i = 0; i <= number; i++){
+    for (let i = 0; i <= (number / 2); i++){
         if ((number % i) === 0 ) {   // !(number % i) am facut asa dar apare si 0 dar impartirea la 0 nu are sens
             console.log(i)
         }
     }
+        console.log(number)
 }
 dividers(120)
