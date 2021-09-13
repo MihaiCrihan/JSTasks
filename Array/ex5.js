94cfb3a9c20f2fa2c31364a2e43a62ce3a337e1c
Mihai  Crihan <32567198+MihaiCrihan@users.noreply.github.com>
2021-09-13 09:47:16 +0300

    Update ex5.js

--- Array/ex5.js
+++ Array/ex5.js
@@ -2,7 +2,9 @@ let arr = [1, 2, 3];
 
 const square = (x) => x * x;
 
-const convert = (fn, array) => array.map(value => fn(value))
+const convert = (fn, array) => array.map(fn)
 
 console.log(convert(square, arr));
 console.log(arr)
+
+//const convert = (fn, array) => array.map(value => fn(value)) pentru 1+parametru
