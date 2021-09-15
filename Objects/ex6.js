bbe6b3a7d6017ac688df95bb45dd55a660b74033
Mihai <haos-sah@mail.ru>
2021-09-15 21:22:17 +0300

    start object module

--- Objects/ex6.js
+++ Objects/ex6.js
@@ -2,7 +2,21 @@
 //     [[1, 2], [3, 4]]
 
 const number = [
-    [1, 2], [3, 4],[null, 4],[3, undefined]
+    [1, 2],
+    [3, 4],
+    [null, 4],
+    [3, undefined]
 ]
+
+number.forEach((item, index) => {
+    for (let i = 0; i < item.length; i++) {
+        if (item[i]) {
+            console.log(item);
+        } else {
+            delete item[i]
+        }
+        console.log(item[i])
+    }
+})
 
 console.log(number)
