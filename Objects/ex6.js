d42474a1d93646a669791c030d213e88362da697
Mihai <cronixm1@gmail.com>
2021-09-21 08:55:15 +0300

    changes

--- Objects/ex6.js
+++ Objects/ex6.js
@@ -1,36 +1,15 @@
 // точки и вернет массив из точек у которых координаты указаны правильно.
 //     [[1, 2], [3, 4]]
 
-let number = [
-    [1, 2],
-    [3, 4],
-    [null, 4],
-    [3, undefined]
+let coordonates = [
+        [1, 2],
+        [3, 4],
+        [null,  4],
+        [3, undefined]
 ]
-//todo de cautat cum de facut singur idee nu am 
-const isNumber = () => {
-    for (let [index, argument] of number.entries()) {
-                console.log(index,argument)
-        // for (const argumentElement of argument) {
-        //
-        // }
-        argument.every(value => {
-            if (!value) {
-                number.splice(index, 1)
-            }
-        })
-    }
+//todo de cautat cum de facut singur idee nu am
+const checkCoordonates = (array) => {
+    return array.filter(item => item[0] && item[1])
 };
-const sampleArray2 = [ 0, -1, -30, 5];
 
-const areNumber = (number, index) => {
-    if (number > 0 ) {
-        return number
-    } else {
-        sampleArray2.splice(index, 1)
-console.log(sampleArray2);
-    }
-}
-console.log(sampleArray2.every(areNumber)); // returns true
-
-// console.log(number
+console.log(checkCoordonates(coordonates))
